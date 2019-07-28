853959ae95de64e8c38c49ba17d636370d544f52
Eiji Kumakawa <morinokumakamesan@gmail.com>
2019-07-28 18:30:50 +0900

    add pages & fix header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,10 @@
 <template>
-  <v-app dark>
+  <v-app light>
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
       :clipped="clipped"
+      :right="right"
       fixed
       app
     >
@@ -24,11 +25,56 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
+
+    <!-- 画面サイズsm以下時のヘッダー -->
     <v-toolbar
       :clipped-left="clipped"
+      color="grey lighten-5"
       fixed
+      flat
       app
+      class="hidden-md-and-up"
     >
+      <v-toolbar-title v-text="title"/>
+      <v-spacer />
+      <v-btn
+        icon
+        @click.stop="drawer = !drawer"
+        class="hidden-md-and-up"
+      >
+      <v-icon>menu</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <!-- 画面サイズmd以上時のヘッダー -->
+    <v-toolbar
+      :clipped-left="clipped"
+      color="grey lighten-5"
+      fixed
+      flat
+      app
+      dense
+      extended
+      class="hidden-sm-and-down"
+    >
+    <v-layout row justify-center>
+      <v-toolbar-title v-text="title"/>
+    </v-layout>
+    <template #extension>
+    <v-layout row justify-center>
+      <v-btn 
+        v-for="(btn, i) in btns"
+        :key="i"
+        :to="btn.to"
+        color="teal darken-1"
+        router
+        flat
+        exact
+      >
+      {{ btn.title }}
+      </v-btn>
+      
+      <!--
       <v-toolbar-side-icon @click="drawer = !drawer" />
       <v-btn
         icon
@@ -48,15 +94,17 @@
       >
         <v-icon>remove</v-icon>
       </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
+      <v-toolbar-title 
+        v-text="title"
+        class="hidden-sm-and-up"
+      />
+      -->
+      <!-- <v-spacer /> -->
+    </v-layout>
+    </template>
     </v-toolbar>
+
+
     <v-content>
       <v-container>
         <nuxt />
@@ -107,10 +155,28 @@ export default {
           to: '/inspire'
         }
       ],
+      btns: [
+        {
+          title: 'Home',
+          to: '/'
+        },
+        {
+          title: 'about',
+          to: '/about'
+        },
+        {
+          title: 'web',
+          to: '/web'
+        },
+        {
+          title: 'cad',
+          to: '/3dcads'
+        }
+      ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'kame\'s portfolio'
     }
   }
 }
